626dd6af08659e581116bfcc072223ea116ebf8a
cgil <lao.tseu.is.alive@gmail.com>
2025-10-23 14:05:37 +0200

    mdi icons now visible

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import {createPinia} from "pinia";
+import '@mdi/font/css/materialdesignicons.css';
 
 const app =createApp(App);
 app.use(createPinia());
